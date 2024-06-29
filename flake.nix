2ee9bb7804ef62395887834c1ca8799ff20f6078
Joel Jinkinson <joel.jinkinson@gmail.com>
2024-06-29 13:48:37 +0100

    Working but scuffed neorg

--- flake.nix
+++ flake.nix
@@ -3,19 +3,21 @@
 
   inputs = {
     # NixOS official package source
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     # Home Manager
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    neorg-overlay.url = "github:nvim-neorg/nixpkgs-neorg-overlay";
     nvim.url = "github:ArgonCat/nvim-nix";
   };
 
-  outputs = { self, nixpkgs, home-manager, nvim, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, nvim, neorg-overlay, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        { nixpkgs.overlays = [ nvim.overlays.default ]; }
+        # I have no clue why you need neorg-overlay here as well, but it works so ehhhhhh TODO
+        { nixpkgs.overlays = [ nvim.overlays.default neorg-overlay.overlays.default ]; }
         ./configuration.nix
         home-manager.nixosModules.home-manager
         {
